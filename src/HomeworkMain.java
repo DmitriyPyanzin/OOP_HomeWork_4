a31dc6097630b0380411f674e6a474bb84088e77
Dmitriy_Pyanzin <lysy_41@mail.ru>
2023-02-13 11:39:38 +0300

    Update2HomeWork

--- src/HomeworkMain.java
+++ src/HomeworkMain.java
@@ -1,23 +1,24 @@
 public class HomeworkMain {
 
     public static void main(String[] args) {
-//        Box<String> stringBox = new Box(); // запрещено!
+//        Box<String> stringBox = new Box<>(); // запрещено!
+
         Box<Apple> appleBox = new Box<>();
         appleBox.add(new Apple("Яблоки", 1.0));
         appleBox.add(new Apple("Яблоки", 2.0));
         appleBox.add(new Apple("Яблоки", 3.0));
-//        apples.add(new Orange(3.0)); // запрещено!!!
+//        appleBox.add(new Orange("Апельсины", 3.0)); // запрещено!!!
         appleBox.add(new GoldenApple("Золотые яблоки", 3.0)); // разрешено
         System.out.println(appleBox.getTitle() + " весят " + appleBox.getSumWeight() + " кг"); // 9.0
 
         Box<Orange> orangeBox = new Box<>();
         orangeBox.add(new Orange("Апельсины", 1.0));
-//        orangeBox.add(new Apple(1.0)); // запрещено!!!
+//        orangeBox.add(new Apple("Яблоки", 1.0)); // запрещено!!!
         System.out.println(orangeBox.getTitle() + " весят " +  orangeBox.getSumWeight() + " кг"); // 1.0
 
         Box<GoldenApple> goldenAppleBox = new Box<>();
         goldenAppleBox.add(new GoldenApple("Золотые яблоки", 2.0));
-//        goldenAppleBox.add(new Apple(2.0)); // запрещено!
+//        goldenAppleBox.add(new Apple("Яблоки", 2.0)); // запрещено!
         System.out.println(goldenAppleBox.getTitle() + " весят " +  goldenAppleBox.getSumWeight() + " кг"); // 2.0
 
 
@@ -25,7 +26,7 @@ public class HomeworkMain {
         System.out.println(goldenAppleBox.getTitle() + " весят " +  goldenAppleBox.getSumWeight() + " кг"); // 0.0
         System.out.println(appleBox.getTitle() + " весят " + appleBox.getSumWeight() + " кг"); // 9.0 + 2.0 = 11.0
 
-//        apples.moveTo(goldenAppleBox); // нельзя!
-//        orangeBox.moveTo(apples); // нельзя
+//        appleBox.moveTo(goldenAppleBox); // нельзя!
+//        orangeBox.moveTo(appleBox); // нельзя
     }
 }
